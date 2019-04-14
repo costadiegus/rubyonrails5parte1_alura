8c9edee6c696a514de275f4923e639befb280b30
DG Costa <costadiegus@gmail.com>
2019-04-14 20:03:04 -0300

    02 Produtos Controller

--- loja/app/controllers/produtos_controller.rb
+++ loja/app/controllers/produtos_controller.rb
@@ -1,2 +1,7 @@
 class ProdutosController < ApplicationController
+    def index
+        @produtos = Produto.order(nome: :desc).limit 2        
+        @produtos_com_menor_preco = Produto.order(:preco).limit 1        
+    end
+    
 end
