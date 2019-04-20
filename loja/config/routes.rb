f8bc1e67a052423464fad2ced7ebdec99bb629e0
DG Costa <costadiegus@gmail.com>
2019-04-20 15:56:17 -0300

    03.02 Formulario de Cadastro

--- loja/config/routes.rb
+++ loja/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "produtos", to: "produtos#index"
   root to: "produtos#index"
+  get "produtos/new", to: "produtos#new"
 end
